9905efc985c43978566c20c99184d4fc26d3e03d
Erimed33 <erikamedina@pursuit.org>
2024-07-03 12:01:57 -0400

    crud-show-works

--- Controllers/catFoodController.js
+++ Controllers/catFoodController.js
@@ -2,12 +2,17 @@ const express = require('express')
 const catFood = express.Router()
 const catFoodInventory = require('../Models/catFoodModels')
 
-
+// GETS all the cat food
 
 catFood.get('/', (req, res) => {
     res.json(catFoodInventory)
 })
 
+// show route - only one cat food
+catFood.get('/:arrayIndex', (req, res) => {
+    const { arrayIndex } = req.params
+    res.json(catFoodInventory[arrayIndex])
+})
 
 // get
 // app.get('/catFood/:index', (req, res) => {
